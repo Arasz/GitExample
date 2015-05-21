19e9fc2fb3b88c80bdea3101e1d6b86c4c9d812d
AraszDev <araszkiewiczrafal@gmail.com>
2015-05-21 19:37:58 +0200

    Git example

--- CSharpAcademy.Git/Program.cs
+++ CSharpAcademy.Git/Program.cs
@@ -10,6 +10,8 @@ namespace CSharpAcademy.Git
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello Git");
+            Console.ReadKey();
         }
     }
 }
